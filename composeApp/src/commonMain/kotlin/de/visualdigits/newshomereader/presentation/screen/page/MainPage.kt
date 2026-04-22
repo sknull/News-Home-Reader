3db45067c1515ca45f75b5d085c31b63111c3ec1
Stephan Knull <s.knull@t-online.de>
2026-04-22 19:23:00 +0200

    Reorganize menubars

--- composeApp/src/commonMain/kotlin/de/visualdigits/newshomereader/presentation/screen/page/MainPage.kt
+++ composeApp/src/commonMain/kotlin/de/visualdigits/newshomereader/presentation/screen/page/MainPage.kt
@@ -10,11 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.unit.dp
